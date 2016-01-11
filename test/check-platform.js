7aeaa3141d87aefe0a324c20366482008f8a6ae7
Rebecca Turner <me@re-becca.org>
2016-01-11 13:44:14 -0800

    src: switch formatting to standard

--- test/check-platform.js
+++ test/check-platform.js
@@ -1,44 +1,44 @@
-var test = require("tap").test
-var c = require("../index.js").checkPlatform
+var test = require('tap').test
+var c = require('../index.js').checkPlatform
 
-test("target cpu wrong", function (t) {
+test('target cpu wrong', function (t) {
   var target = {}
-  target.cpu = "enten-cpu"
-  target.os = "any"
+  target.cpu = 'enten-cpu'
+  target.os = 'any'
   c(target, false, function (err) {
-    t.ok(err, "error present")
-    t.equal(err.code, "EBADPLATFORM")
+    t.ok(err, 'error present')
+    t.equal(err.code, 'EBADPLATFORM')
     t.end()
   })
 })
 
-test("os wrong", function (t) {
+test('os wrong', function (t) {
   var target = {}
-  target.cpu = "any"
-  target.os = "enten-os"
+  target.cpu = 'any'
+  target.os = 'enten-os'
   c(target, false, function (err) {
-    t.ok(err, "error present")
-    t.equal(err.code, "EBADPLATFORM")
+    t.ok(err, 'error present')
+    t.equal(err.code, 'EBADPLATFORM')
     t.end()
   })
 })
 
-test("nothing wrong", function (t) {
+test('nothing wrong', function (t) {
   var target = {}
-  target.cpu = "any"
-  target.os = "any"
+  target.cpu = 'any'
+  target.os = 'any'
   c(target, false, function (err) {
-    t.notOk(err, "no error present")
+    t.notOk(err, 'no error present')
     t.end()
   })
 })
 
-test("force", function (t) {
+test('force', function (t) {
   var target = {}
-  target.cpu = "enten-cpu"
-  target.os = "any"
+  target.cpu = 'enten-cpu'
+  target.os = 'any'
   c(target, true, function (err) {
-    t.notOk(err, "no error present")
+    t.notOk(err, 'no error present')
     t.end()
   })
 })
