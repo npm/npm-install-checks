0a989177ee6bc9258e2ea425ab44a16a1ae3c23e
nlf <quitlahok@gmail.com>
2023-03-20 13:53:16 -0700

    feat: support libc field checks

--- test/check-platform.js
+++ test/check-platform.js
@@ -42,3 +42,81 @@ t.test('os wrong (negation)', async t =>
 
 t.test('nothing wrong (negation)', async t =>
   checkPlatform({ cpu: '!enten-cpu', os: '!enten-os' }))
+
+t.test('libc', (t) => {
+  let PLATFORM = ''
+
+  const _processPlatform = Object.getOwnPropertyDescriptor(process, 'platform')
+  Object.defineProperty(process, 'platform', {
+    enumerable: true,
+    configurable: true,
+    get: () => PLATFORM,
+  })
+
+  let REPORT = {}
+  const _processReport = process.report.getReport
+  process.report.getReport = () => REPORT
+
+  t.teardown(() => {
+    Object.defineProperty(process, 'platform', _processPlatform)
+    process.report.getReport = _processReport
+  })
+
+  t.test('fails when not in linux', (t) => {
+    PLATFORM = 'darwin'
+
+    t.throws(() => checkPlatform({ libc: 'glibc' }), { code: 'EBADPLATFORM' },
+      'fails for glibc when not in linux')
+    t.throws(() => checkPlatform({ libc: 'musl' }), { code: 'EBADPLATFORM' },
+      'fails for musl when not in linux')
+    t.end()
+  })
+
+  t.test('glibc', (t) => {
+    PLATFORM = 'linux'
+
+    REPORT = {}
+    t.throws(() => checkPlatform({ libc: 'glibc' }), { code: 'EBADPLATFORM' },
+      'fails when report is missing header property')
+
+    REPORT = { header: {} }
+    t.throws(() => checkPlatform({ libc: 'glibc' }), { code: 'EBADPLATFORM' },
+      'fails when header is missing glibcRuntimeVersion property')
+
+    REPORT = { header: { glibcRuntimeVersion: '1' } }
+    t.doesNotThrow(() => checkPlatform({ libc: 'glibc' }), 'allows glibc on glibc')
+    t.throws(() => checkPlatform({ libc: 'musl' }), { code: 'EBADPLATFORM' },
+      'does not allow musl on glibc')
+
+    t.end()
+  })
+
+  t.test('musl', (t) => {
+    PLATFORM = 'linux'
+
+    REPORT = {}
+    t.throws(() => checkPlatform({ libc: 'musl' }), { code: 'EBADPLATFORM' },
+      'fails when report is missing sharedObjects property')
+
+    REPORT = { sharedObjects: {} }
+    t.throws(() => checkPlatform({ libc: 'musl' }), { code: 'EBADPLATFORM' },
+      'fails when sharedObjects property is not an array')
+
+    REPORT = { sharedObjects: [] }
+    t.throws(() => checkPlatform({ libc: 'musl' }), { code: 'EBADPLATFORM' },
+      'fails when sharedObjects does not contain musl')
+
+    REPORT = { sharedObjects: ['ld-musl-foo'] }
+    t.doesNotThrow(() => checkPlatform({ libc: 'musl' }), 'allows musl on musl as ld-musl-')
+
+    REPORT = { sharedObjects: ['libc.musl-'] }
+    t.doesNotThrow(() => checkPlatform({ libc: 'musl' }), 'allows musl on musl as libc.musl-')
+
+    t.throws(() => checkPlatform({ libc: 'glibc' }), { code: 'EBADPLATFORM' },
+      'does not allow glibc on musl')
+
+    t.end()
+  })
+
+  t.end()
+})
